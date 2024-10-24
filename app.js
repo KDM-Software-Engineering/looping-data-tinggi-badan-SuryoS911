d43388db41f119bb02871bf8b402529a0422c748
SuryoS911 <suryo237658@student.smkn1kandeman.sch.id>
2024-10-24 14:14:22 +0700

    selesai

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 function runner() {
-
   // TULIS SCRIPT KAMU DI SINI
-
-
-
-
-
-
+  const tinggiBadan = ["150", "155", "148", "165", "170", "168", "166"]
+  const namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
+  for (let i = 0; i < tinggiBadan.length; i++) {
+    console.log('Atlet '+namaAtlet[i]+' tinggi badannya '+tinggiBadan[i]+'cm')
+  }
   /// JANGAN UBAH SELAINNYA
 }
 
